06fa9250cef8acc7f086b484b2743480b2321c2b
EPICZEUS <s77broz@aol.com>
2018-11-05 10:22:45 -0500

    Add solution

--- app/models/venture_capitalist.rb
+++ app/models/venture_capitalist.rb
@@ -1,3 +1,50 @@
 class VentureCapitalist
+	@@all = []
 
+	def self.all
+		@@all
+	end
+
+	def self.tres_commas_club
+		@@all.select{|venture| venture.total_worth >= 1000000000 }
+	end
+
+	attr_accessor :total_worth
+	attr_reader :name
+
+	def initialize(name, worth)
+		@name = name
+		@total_worth = worth
+
+		@@all << self
+	end
+
+	def offer_contract(startup, type, amount)
+		FundingRound.new(type, amount, startup, self)
+	end
+
+	def funding_rounds
+		FundingRound.all.select{|round| round.venture_capitalist == self }
+	end
+
+	def portfolio
+		funding_rounds.map{|round| round.startup }.uniq
+	end
+
+	def biggest_investment
+		high = funding_rounds.first
+
+		funding_rounds.each do |round|
+			if round.investment > high.investment
+				high = round
+			end
+		end
+
+		high
+	end
+
+	def invested(domain)
+		domain_rounds = funding_rounds.select{|round| round.startup.domain == domain }
+		domain_rounds.map{|round| round.investment }.reduce(:+)
+	end
 end
--- tools/console.rb
+++ tools/console.rb
@@ -6,6 +6,13 @@ end
 # Insert code here to run before hitting the binding.pry
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
+s1 = Startup.new("Twotter", "Sam", "social media")
+s2 = Startup.new("EyeStory", "Mork Zackerbug", "social media")
+s3 = Startup.new("CombatSetting", "Analog Slights", "video game")
+
+c1 = VentureCapitalist.new("John Lennon", rand(10349818947289147) + 100000)
+c2 = VentureCapitalist.new("Mark Ruffalo", rand(10349818947289147) + 100000)
+c3 = VentureCapitalist.new("Sir Isaac Newton", rand(10349818947289147) + 100000)
 
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
