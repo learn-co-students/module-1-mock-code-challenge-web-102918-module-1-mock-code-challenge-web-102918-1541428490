f5e3705b15885141da298420d9b924746dfe8c59
ruthmesfun <ruth.mesfun@gmail.com>
2018-11-05 10:41:02 -0500

    Almost done, I need to create a pivot find founder and invested methods

--- app/models/funding_round.rb
+++ app/models/funding_round.rb
@@ -1,3 +1,38 @@
 class FundingRound
+    attr_reader :startup, :venture_capitalist
+    attr_accessor :type, :investment
+    @@all = []
 
+    def initialize(startup, venture_capitalist, type, investment)
+       
+        if investment > 0 
+            @investment = investment.to_f
+            @startup = startup
+            @type = type
+            @venture_capitalist = venture_capitalist
+            self.class.all << self
+        else
+            puts "There was an error in creating this form please try again"
+        end
+    end
+
+    def self.all
+        @@all 
+    end
+
+    def self.domains
+        domains_invested = {}
+
+        all.each do |funding_round|
+            if domains_invested[funding_round.startup.domain]
+                domains_invested[funding_round.startup.domain] += funding_round.investment 
+                domains_invested[funding_round.startup.domain]
+            else 
+                domains_invested[funding_round.startup.domain] = funding_round.investment
+            end
+        end
+
+        domains_invested
+    end
+    
 end
--- app/models/venture_capitalist.rb
+++ app/models/venture_capitalist.rb
@@ -1,3 +1,49 @@
 class VentureCapitalist
+    attr_reader :name, :total_worth
+    @@all = []
+
+    def self.all
+        @@all
+    end
+
+    def initialize(name, total_worth)
+        @total_worth = total_worth
+        @name = name
+        self.class.all << self
+    end
+
+    def tres_commas_club
+        total_worth > 1000000000
+    end
+
+    def self.tres_commas_club
+        all.select{|capitalist| capitalist.tres_commas_club}
+    end
+
+    def offer_contract(startup, type, amount_invested)
+        FundingRound.new(startup, self, type, amount_invested)
+    end
+
+    def funding_rounds
+        FundingRound.all.select{|funding_round|funding_round.venture_capitalist == self}
+    end
+
+    def portfolio
+        list_of_startups = funding_rounds.map{|funding_round| funding_round.startup}
+
+        list_of_startups.uniq
+    end
+
+    def sorted_investment
+        funding_rounds.sort{|funding_roundA, funding_roundB| funding_roundB.investment <=> funding_roundA.investment }
+    end
+
+    def biggest_investment
+        sorted_investment.first
+    end
+
+    # def invested 
+    #     funding_rounds
+    # end
 
 end
