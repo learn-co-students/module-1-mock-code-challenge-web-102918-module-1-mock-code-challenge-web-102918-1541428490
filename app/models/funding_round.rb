09a43dcbcc81294b7454bdf86f21aba3b8e6cb8c
Henry <hankeliu2015@users.noreply.github.com>
2018-11-06 00:14:33 -0500

    Update funding_round.rb

--- app/models/funding_round.rb
+++ app/models/funding_round.rb
@@ -1,15 +1,19 @@
 class FundingRound
-
- attr_reader :startup, :venture_capitalist
+  attr_reader :startup, :venture_capitalist
   attr_accessor :round_type, :investment
 
   @@all = []
 
-  def initialize(startup, venture_capitalistc, round_type, investment)
+  def initialize(startup, venture_capitalist, round_type, investment)
     @startup = startup
-    @vc = vc
+    @venture_capitalist = venture_capitalist
     @type = type
-    @invest = invest
+    @investment = investment.to_f
     @@all << self
   end
+
+  def self.all
+    @@all
+  end
+
 end
