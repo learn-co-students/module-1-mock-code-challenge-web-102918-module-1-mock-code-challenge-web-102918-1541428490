43d4cbb69ade40a9b7f55c6935c977b72ae88c63
Henry <hankeliu2015@users.noreply.github.com>
2018-11-06 00:12:22 -0500

    Update startup.rb

--- app/models/startup.rb
+++ app/models/startup.rb
@@ -3,8 +3,10 @@ class Startup
   attr_reader :founder, :domain
   @@all = []
 
-  def initialize(name)
+  def initialize(name, founder, domain)
     @name = name
+    @founder = founder
+    @domain = domain
     @@all << self
   end
 
@@ -18,11 +20,8 @@ class Startup
   end
 
   def find_by_founder(founder)
-
     @@all.find do |startup|
-      if startup.name == founder
-        startup
-      end
+      startup.name == founder
     end
   end
 
@@ -31,4 +30,26 @@ class Startup
       startup.domain
     end
   end
-end
+
+  def sign_contract(venture_capitalist, round_type, investment)
+    FoundingRound.new(self,venture_capitalist, round_type, investment )
+  end
+
+  def num_funding_rounds
+    counter = 0
+    FundingRound.all.each do |round|
+      round.startup == self
+      counter += 1
+    end
+    counter
+  end
+
+  def total_funds
+    total_fund = 0
+    FundingRound.all. each do |round|
+      if round.startup == self
+        total_funds += round.investment
+      end
+    end
+    total_funds
+  end
